35666c59078510a7e848842f5d6298c4b71bae4e
Shmulls-Git <sshmoel1@gmail.com>
2021-11-09 16:44:50 +0200

    New update

--- MedicTouch/Main.c
+++ MedicTouch/Main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
+void main_menu();
+void login();
+
 int main()
 {
-	printf("Hello wG\n");
+	main_menu();
+	return 0;
+}
+
+void main_menu()
+{
+	int menu_select;
+
+	printf("Hello welcome to MedicTouch clinic appointment management system\n");
+	printf("Please select the operation:\n");
+	printf(" [1] Create user\n [2] Login to the system\n [3] Password recovery\n [4] Exit\n");
+	scanf_s("%d", &menu_select);
+
+	switch (menu_select)
+	{
+		case 1:
+		{
+		login();
+		break;
+		}
+	}
+
+}
+
+void login()
+{
+	printf("Good work!\n");
 }
