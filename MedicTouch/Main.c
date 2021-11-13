f84817d1c4981b1501d56e29a7a8011401e2846e
Shmulls-Git <sshmoel1@gmail.com>
2021-11-13 20:15:35 +0200

    Ver 2.1

--- MedicTouch/Main.c
+++ MedicTouch/Main.c
@@ -11,25 +11,60 @@ typedef struct doctor
 
 void create_user()
 {
-	FILE* fic = fopen("Doctor.csv", "r+");
-	if (fic == NULL)
-	exit(1);
+	int selection;
+	printf("Choose:\n[1]Patient\n[2]Doctor\n");
+	scanf("%d", &selection);
 
-	doctor data;
+	switch (selection)
+	{
+	case 1:
+	{
+		FILE* p1 = fopen("Patient.csv", "r+");
+		if (p1 == NULL)
+			exit(1);
 
-	printf("enter user name: ");
-	scanf("%s", data.user_name);
+		doctor data;
 
-	printf("Enter password: ");
-	scanf("%s", data.password);
+		printf("enter user name: ");
+		scanf("%s", data.user_name);
 
-	//int name = 0, age = 0;
-	//fscanf(fic, "%d;%d", &name, &age);
-	fseek(fic, 0, SEEK_END);
-	fprintf(fic, "%s,%s\n", data.user_name, data.password);
-	printf("username: %s\npassword: %s\n", data.user_name, data.password);
+		printf("Enter password: ");
+		scanf("%s", data.password);
 
-	fclose(fic);
+		//int name = 0, age = 0;
+		//fscanf(fic, "%d;%d", &name, &age);
+		fseek(p1, 0, SEEK_END);
+		fprintf(p1, "%s,%s\n", data.user_name, data.password);
+		printf(">Welcome to MedicTouch %s<\n", data.user_name);
+
+		fclose(p1);
+		break;
+	}
+	
+	case 2:
+	{
+		FILE* p2 = fopen("Doctor.csv", "r+");
+		if (p2 == NULL)
+			exit(1);
+
+		doctor data;
+
+		printf("enter user name: ");
+		scanf("%s", data.user_name);
+
+		printf("Enter password: ");
+		scanf("%s", data.password);
+
+		//int name = 0, age = 0;
+		//fscanf(fic, "%d;%d", &name, &age);
+		fseek(p2, 0, SEEK_END);
+		fprintf(p2, "%s,%s\n", data.user_name, data.password);
+		printf(">Welcome to MedicTouch Dr.%s<\n", data.user_name);
+
+		fclose(p2);
+		break;
+	}
+	}
 }
 
 int main()
@@ -38,13 +73,14 @@ int main()
 
 	do
 	{
-		printf("choose operation: ");
+		printf("Choose operation:\n[1]Create user\n[2]Login\n[3]Password recovery\n[0]Exit\n");
 		scanf("%d", &menu);
 
 		switch (menu)
 		{
 			case 1: 
 				create_user();
+				break;
 		}
 
 	} while (menu != 0);
